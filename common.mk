a8cd743db0473b680fdae32b608fd4808c558012
secuflag <secuflag@ticino.com>
2019-09-03 12:05:05 +0200

    Add settings for ATV target

--- common.mk
+++ common.mk
@@ -5,20 +5,6 @@
 MOD_VERSION := PIE-R.02
 AOSP_TAG_ID := android-9.0.0_r46
 
-# OpenGapps
-GAPPS_VARIANT := pico
-
-GAPPS_PRODUCT_PACKAGES += \
-    CalendarGooglePrebuilt \
-    Chrome \
-    LatinImeGoogle \
-    PrebuiltGmail
-
-GAPPS_EXCLUDED_PACKAGES := \
-    CalendarGooglePrebuilt \
-    GooglePackageInstaller \
-    PrebuiltGmail
-
 # Common overlay
 PRODUCT_PACKAGE_OVERLAYS += \
     vendor/secuflag/overlay/
@@ -39,17 +25,38 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_COPY_FILES += \
     vendor/secuflag/extra/hosts:system/etc/hosts
 
-# Terminal
+# Add extra apps to non-ATV target
+ifneq ($(TARGET_BUILD_GOOGLE_ATV), true)
 PRODUCT_PACKAGES += \
-    Terminal
-
-# Eleven
-# https://github.com/LineageOS/android_packages_apps_Eleven
-PRODUCT_PACKAGES += \
+    Terminal \
     Eleven
+endif
+
+# OpenGapps - non-ATV target
+ifneq ($(TARGET_BUILD_GOOGLE_ATV), true)
+GAPPS_VARIANT := pico
+
+GAPPS_PRODUCT_PACKAGES += \
+    CalendarGooglePrebuilt \
+    Chrome \
+    LatinImeGoogle \
+    PrebuiltGmail
+
+GAPPS_EXCLUDED_PACKAGES := \
+    CalendarGooglePrebuilt \
+    GooglePackageInstaller \
+    PrebuiltGmail
+
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
+endif
+
+# OpenGapps - ATV target
+ifeq ($(TARGET_BUILD_GOOGLE_ATV), true)
+PRODUCT_PACKAGES += TvProvision
+include vendor/google/atv/atv-vendor.mk
+endif
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mod.version=$(MOD_VERSION) \
     ro.aosptag.id=$(AOSP_TAG_ID)
 
-$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
