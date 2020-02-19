76aaa46ce042ecefca359c4f643e0bdb6a568061
Davide P <secuflag@users.noreply.github.com>
2020-02-19 10:10:17 +0100

    Add curl binary as extra package

--- common.mk
+++ common.mk
@@ -25,6 +25,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.service.adb.enable=1
 
+# Extra packages
+PRODUCT_PACKAGES += \
+    curl
+
 # Add extra apps to non-ATV target
 ifneq ($(TARGET_BUILD_GOOGLE_ATV), true)
 PRODUCT_PACKAGES += \
@@ -49,4 +53,3 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     ro.mod.version=$(MOD_VERSION) \
     ro.aosptag.id=$(AOSP_TAG_ID)
-
